6741b7b45d9274a4b095965a5bf1fa119e2c2363
zhangjianxin1 <zhang-rd-fe@s139.org>
2017-03-25 05:24:34 +0800

    更新概要： 1.初始化项目，并且完善基本功能。

---
+++ src/main/java/com/yhxd/tools/XmlExercise.java
@@ -0,0 +1,50 @@
+package com.yhxd.tools;
+import net.sf.json.JSON;
+import net.sf.json.JSONSerializer;
+import net.sf.json.xml.XMLSerializer;
+
+import org.jdom.Document;
+
+
+/**
+ * Created by zhangjianxin on 2017/3/25.
+ */
+public class XmlExercise {
+
+    /**
+     * 将xml字符串<STRONG>转换</STRONG>为JSON字符串
+     *
+     * @param xmlString
+     *            xml字符串
+     * @return JSON<STRONG>对象</STRONG>
+     */
+    public static String xml2json(String xmlString) {
+        XMLSerializer xmlSerializer = new XMLSerializer();
+        JSON json = xmlSerializer.read(xmlString);
+        return json.toString(1);
+    }
+
+    /**
+     * 将xmlDocument<STRONG>转换</STRONG>为JSON<STRONG>对象</STRONG>
+     *
+     * @param xmlDocument
+     *            XML Document
+     * @return JSON<STRONG>对象</STRONG>
+     */
+    public static String xml2json(Document xmlDocument) {
+        return xml2json(xmlDocument.toString());
+    }
+
+    /**
+     * JSON(数组)字符串<STRONG>转换</STRONG>成XML字符串
+     *
+     * @param jsonString
+     * @return
+     */
+    public static String json2xml(String jsonString) {
+        XMLSerializer xmlSerializer = new XMLSerializer();
+        return xmlSerializer.write(JSONSerializer.toJSON(jsonString));
+        // return xmlSerializer.write(JSONArray.fromObject(jsonString));//这种方式只支持JSON数组
+    }
+
+}
